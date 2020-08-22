e391395755d241e68cba04f14b7f25a140d66823
iamcid <iamcid17@gmail.com>
2020-08-22 03:08:23 -0400

    wires up login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
 
-    def home
-    end
+    # def home
+    # end
 
     def destroy
         session.clear
         redirect_to root_path
+    end
+
+    def create
+        therapist = Therapist.find_by(email: params[:therapist][:email])
+        if therapist && therapist.authenticate(params[:therapist][:password])
+            session[:therapist_id] = therapist.id
+            redirect_to therapist_path(therapist)
+        else
+            flash[:message] = "Invalid credentials. Please try again."
+            redirect_to "/login"
+        end
     end
 
 end
